f75d2cb26b3adcc76834ae29971b1357f94ed6c9
raresmigea <raresini@yahoo.com>
2021-03-02 00:25:03 +0200

    updated api response

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 var router = Router();
 
 router.get('/', function (req, res, next) {
-  res.send('API is working properly');
+  res.send('API is working properly!');
 });
 
 export default router;
